a2fc65708fc4ba7c14b2df8610a0dc865bb5d01e
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 18:34:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,7 +25,7 @@ def reverse_array(arr)
 end
 
 def kesha_maker(arr a)
-  blake = arr[0].replace('s', '$')
+  blake = arr[0].replace('s', a)
   ashley = arr[1]
   scott = arr[2]
   [blake, scott, ashley]
